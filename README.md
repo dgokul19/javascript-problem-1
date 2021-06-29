306e1d851a91b6e70ff7dfac7bc9b76aeba4c5b7
dgokul19 <dgokul19@gmail.com>
2021-06-29 09:41:39 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# javascript-problem-1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/javascript-problem-1)
---
+++ index.js
@@ -0,0 +1,18 @@
+// Find each uninque value and its length;
+
+const givenArray = ['a','e','v','s','s','c','c','a','v','e','a','v'];
+
+
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+
+let result = {};
+
+givenArray.forEach(value => {
+  result[value] =  result[value] ? result[value] + 1 : 1;
+});
+
+appDiv.innerHTML = `<p>${JSON.stringify(result)}</p>`;
